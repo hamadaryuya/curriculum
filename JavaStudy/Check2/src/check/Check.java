788566f3db0aa62cc6dd44b59a2a50cb8ce3c1b8
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2020-12-09 17:41:18 +0900

    第２章チェックテストの提出

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -3,18 +3,20 @@ package check;
 import constants.Constants;
 
 public class Check {
-	
+	//課題①
 	private static String firstName = "濱田";
 	private static String lastName = "龍也";
 	
+	//課題②
 	private void printName(String firstName, String lastName) {
 		String sumName = firstName + lastName;
 		System.out.println("printNameメソッド  → " + sumName);
 	}
-	
 	public static void main(String[] args) {
 		Check nm = new Check();
 		nm.printName(firstName,lastName);
+		//課題③
+		
 		Pet pt = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		pt.introduce();
