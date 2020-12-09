2ced93f1cc175bea931046585bc77af7b8d48086
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2020-12-09 17:36:39 +0900

    第２章チェックテストの提出

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,5 +1,7 @@
 package check;
 
+import constants.Constants;
+
 public class Check {
 	
 	private static String firstName = "濱田";
@@ -13,8 +15,10 @@ public class Check {
 	public static void main(String[] args) {
 		Check nm = new Check();
 		nm.printName(firstName,lastName);
+		Pet pt = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		pt.introduce();
+		rp.introduce();
 	}
-	
-	
 }
 
